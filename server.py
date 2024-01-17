4b78fabc066cb00c1b88fc6864678ca451b70040
Tim Hanewich <tahanewich@live.com>
2024-01-17 12:56:06 -0500

    Print received data

--- server.py
+++ server.py
@@ -12,6 +12,11 @@ while True:
     print("Connection from " + str(addr) + "!")
     data:bytes = conn.recv(1024)
     print(str(len(data)) + " bytes received!")
+
+    # print the content
+    print("Content received: " + str(data))
+
+    # send 200 OK
     conn.send("HTTP/1.0 200 OK\r\n\r\n".encode("utf-8"))
     conn.close()
 
